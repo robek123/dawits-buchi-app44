86d2169f56a42e469687c9f54962ecd75671a27d
robek123 <robek2016@gmail.com>
2023-05-27 10:17:09 +0300

    Update File

--- app/main.py
+++ app/main.py
@@ -1,3 +1,12 @@
+@app.post('/create_pet')
+async def create_pet(pet_type: str, pet_name: str, status: str):
+    new_pet = {
+        'pet_type': pet_type,
+        'pet_name': pet_name,
+        'status': status
+    }
+    pet_id = collection.insert_one(new_pet).inserted_id
+    return {'message': f'Pet created with id: {pet_id}'}
 
 
 @app.get('/get_all_pets')
