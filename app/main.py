de220997a3309beacf1ba601232726123e280e15
robek123 <robek2016@gmail.com>
2023-05-27 09:54:57 +0300

    Update File

--- app/main.py
+++ app/main.py
@@ -1,24 +1,8 @@
 
 
-from fastapi import FastAPI
-from pydantic import BaseModel
-from pymongo import MongoClient
-
-app = FastAPI()
-
-client = MongoClient("mongodb://mongo:27017")
-db = client['pets_db']
-collection = db['pets_collection']
-
-
-class Pet(BaseModel):
-    name: str
-    pet_type: str
-    status: str
-
-
-@app.post('/create_pet')
-async def create_pet(pet: Pet):
-    pet_doc = pet.dict()
-    insert_result = collection.insert_one(pet_doc)
-    return {"message": "Pet created successfully", "pet_id": str(insert_result.inserted_id)}
+@app.get('/get_all_pets')
+async def get_all_pets():
+    pets_list = []
+    for pet in collection.find():
+        pets_list.append(pet)
+    return pets_list
